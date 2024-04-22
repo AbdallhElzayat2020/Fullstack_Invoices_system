c7b4e440ada99d757ac746990058aacae24b5f94
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-04-22 11:04:50 +0200

    home page dynamic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\invoices;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,45 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //total of Invoices
+        $totalInvoices = number_format(invoices::sum('Total'), 2);
+        //Count of invoices
+        $invoicesCount = invoices::count();
+        ///////////////////////////////////
+        //total of invoicesNotPay
+        $invoicesNotPay = invoices::where('Value_Status', '2')->sum('Total');
+        $CountInvoicesNotPay = invoices::where('Value_Status', '2')->count();
+        ///////////////////////////////////
+        //percentage of NotPay Invoices
+        /* notPayInvoices / countTotal Invoices *100  */
+        $percentNotPay = round($CountInvoicesNotPay / $invoicesCount * 100, 2);
+        ///////////////////////////////////
+
+        //Payment Invoices
+        $CountPayInvoices = invoices::where('Value_Status', '1')->count();
+        $SumPayMentInvoices = invoices::where('Value_Status', '1')->sum('Total');
+        /* PayInvoices / countTotal Invoices *100  */
+        $percentPay = round($CountPayInvoices / $invoicesCount * 100, 2);
+
+        ////////////////////////////////////////////////////////
+        //Payment Invoices
+        $CountPartiallypaid = invoices::where('Value_Status', '3')->count();
+        $SumPartiallypaid = invoices::where('Value_Status', '3')->sum('Total');
+        /* PayInvoices / countTotal Invoices *100  */
+        $percentPartiallypaid = round($CountPartiallypaid / $invoicesCount * 100, 2);
+
+        return view('home', compact(
+            'totalInvoices',
+            'invoicesCount',
+            'invoicesNotPay',
+            'CountInvoicesNotPay',
+            'percentNotPay',
+            'CountPayInvoices',
+            'SumPayMentInvoices',
+            'percentPay',
+            'CountPartiallypaid',
+            'SumPartiallypaid',
+            'percentPartiallypaid'
+        ));
     }
 }
