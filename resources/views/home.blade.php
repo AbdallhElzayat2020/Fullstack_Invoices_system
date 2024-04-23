9c47e221c67222e0b0c221cf0deaf4e499b574bb
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-04-23 08:53:12 +0200

    edit HomePage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,22 +18,22 @@
             </div>
         </div>
         <div class="main-dashboard-header-right">
-            <div>
+            {{-- <div>
                 <label class="tx-13">Customer Ratings</label>
                 <div class="main-star">
                     <i class="typcn typcn-star active"></i> <i class="typcn typcn-star active"></i> <i
                         class="typcn typcn-star active"></i> <i class="typcn typcn-star active"></i> <i
                         class="typcn typcn-star"></i> <span>(14,873)</span>
                 </div>
-            </div>
-            <div>
+            </div> --}}
+            {{-- <div>
                 <label class="tx-13">Online Sales</label>
                 <h5>563,275</h5>
-            </div>
-            <div>
+            </div> --}}
+            {{-- <div>
                 <label class="tx-13">Offline Sales</label>
                 <h5>783,675</h5>
-            </div>
+            </div> --}}
         </div>
     </div>
     <!-- /breadcrumb -->
